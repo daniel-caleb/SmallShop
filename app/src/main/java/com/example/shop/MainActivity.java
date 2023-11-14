ff81ffad90ed93f8716d336e003b584a792b1d30
Ronoh <95380895+daniel-caleb@users.noreply.github.com>
2023-11-14 16:46:40 +0300

    Button Functionality 1.0

--- app/src/main/java/com/example/shop/MainActivity.java
+++ app/src/main/java/com/example/shop/MainActivity.java
@@ -3,13 +3,16 @@ package com.example.shop;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int MAX_QUANTITY = 4;
     Button button,button2,button3,button4,button5,button6,button7;
     EditText editTextText,editTextText2,editTextText3,editTextText4,editTextText5,editTextText6,editTextText7,editTextText8,editTextText9;
     EditText editTextText10,editTextText11,editTextText12,editTextText13,editTextText14,editTextText15;
+    private int itemCount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +43,34 @@ public class MainActivity extends AppCompatActivity {
         editTextText14=findViewById(R.id.editTextText14);
         editTextText15=findViewById(R.id.editTextText15);
 
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (itemCount < MAX_QUANTITY) {
+                    itemCount++;
+                    updateTotalPrice();
+                }                
+            }
+        });
+    }
 
+    private void updateTotalPrice() {
+        double totalPrice = itemCount * getPriceForItem("Milk");
+        editTextText.setText("Kshs." + String.format("%.2f", totalPrice));
+    }
+
+    private double getPriceForItem(String itemName) {
+        switch (itemName) {
+            case "Milk":
+                return 60.00;
+            case "Sugar":
+                return 240.00;
+            case "Flour":
+                return 130.00;
+            case "Bread":
+                return 65.00;
+            default:
+                return 0.00;
+        }
     }
 }
